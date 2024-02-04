525f7d1b07dd0666cac837da88ebdabb1ccf7af5
siyoon <ryuu.public@gmail.com>
2024-02-04 16:01:49 +0900

    feat: integrate with backend server

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
 import logo from './logo.svg';
+import Todo from './Todo';
+import AddTodo from './AddTodo';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { Container, List, Paper } from '@mui/material';
+import { call } from './service/ApiService';
 
 function App() {
+  const [items, setItems] = useState([
+    // {
+    //   id: "0",
+    //   title: "Hello World 1",
+    //   done: true
+    // },
+    // {
+    //   id: "1",
+    //   title: "Hello World 2",
+    //   done: false
+    // }
+  ]); 
+  
+  // useEffect(() => {
+  //   const requestOptions = {
+  //     method: "GET",
+  //     headeres: {"Content-Type": "application/json"},
+  //   }
+
+  //   fetch("http://localhost:8080/todo", requestOptions)
+  //   .then((response) => response.json())
+  //   .then(
+  //     (response) => {
+  //       setItems(response.data);
+  //     },
+  //     (error) => {
+
+  //     }
+  //   )
+  // }, []);
+
+  useEffect(() => {
+    call("/todo", "GET", null)
+    .then((response) => setItems(response.data));
+  }, []);
+
+  const addItem = (item) => {
+    call("/todo", "POST", item)
+    .then((response) => setItems(response.data));
+  }
+
+  const editItem = (item) => {
+    call("/todo", "PUT", item)
+    .then((response) => setItems(response.data));
+  }
+
+  const deleteItem = (item) => {
+    call("/todo", "DELETE", item)
+    .then((response) => setItems(response.data));
+  }
+
+  let todoItems = items.length > 0 && (
+    <Paper style={{ margin: 16 }}>
+      <List>
+        {items.map((item) => (
+            <Todo item={item} key={item.id} editItem={editItem} deleteItem={deleteItem}/>)
+          )
+        } 
+      </List>
+    </Paper>
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container maxWidth="md">
+        <AddTodo addItem={addItem} />
+        <div className='TodoList'>{todoItems}</div>
+      </Container>
     </div>
   );
 }
